b72fdb528585b0c7598a57098ec89f113043fd6c
jimao <jimao@jimaodeMacBook-Pro.local>
2023-03-18 14:17:47 +0800

    提交代码

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Emoji, INFT } from './Emoji';
 import style from './View.module.scss';
-import { useContract, useContractRead } from 'wagmi';
+import { useContract, useContractRead, usePrepareContractWrite, useContractWrite } from 'wagmi';
 import EmojiInterface from '../contract/Emoji/interface.json';
+import { Button } from 'antd';
 
 const EmojiContract = {
   address: '0x70C38C4279ca77020CB0551465d0783011D869ca',
@@ -24,7 +25,7 @@ function View() {
     focus[1] + 5,
   ];
 
-  const [nfts, setNfts] = useState<INFT[][]>([]);
+  // const [nfts, setNfts] = useState<INFT[][]>([]);
 
   useEffect(() => {
     // const nfts = Array(11).fill(0)
@@ -69,13 +70,61 @@ function View() {
     address: EmojiContract.address as any,
     abi: EmojiContract.abi,
     functionName: 'name',
+    keepPreviousData: true,
   });
 
+  const getNFTsInRectangleResult = useContractRead({
+    address: EmojiContract.address as any,
+    abi: EmojiContract.abi,
+    functionName: 'getNFTsInRectangle',
+    args: currentArea(),
+  });
+
+  const nfts = () => {
+    const result = (getNFTsInRectangleResult.data || []) as any[][];
+    return result.map((row) => row.map((data) => ({
+      x: Number(data['x']),
+      y: Number(data['y']),
+      stringData: data['stringData'],
+    })));
+  };
+
+
+  // const { config } = usePrepareContractWrite({
+  //   address: EmojiContract.address as any,
+  //   abi: EmojiContract.abi,
+  //   functionName: 'mint',
+  //   args: [{
+  //     stringData: '🌳',
+  //     targetAddress: '',
+  //     x: 0,
+  //     y: 0,
+  //     tokenURI: '',
+  //   }],
+  // });
+  // const wr = useContractWrite(config);
+
   return <div className={style.com}>
-    {nfts.map((row) => <div className={style.row}>
+    {nfts().map((row) => <div className={style.row}>
       {row.map((nft) => <Emoji { ...nft } />)}
     </div>)}
+    <p>
     <span>{currentArea().join(',')}</span>
+    </p>
+    <p>
     <span>{data as any}</span>
+    </p>
+    <p>
+      {/* {JSON.stringify(a?.data)} */}
+    </p>
+    {/* <p>
+      {JSON.stringify(wr?.data)}
+    </p> */}
+    <p>
+      <Button onClick={async () => {
+        // wr.write?.();
+        console.log(nfts());
+      }}>Mint</Button>
+    </p>
   </div>;
 }
